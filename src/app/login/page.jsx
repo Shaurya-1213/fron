b9124e3c140ff65f75f40b2875bb3f2ca8088f87
Shaurya Singh <163731296+Shaurya-1213@users.noreply.github.com>
2024-06-10 09:11:06 -0400

    Update page.jsx

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -13,7 +13,7 @@ const Login = () => {
         <input className={classes.mainInput} type="password" placeholder='password' />
         <input className={classes.mainInput} type="password" placeholder='password' />
         <input className={classes.mainInput} type="password" placeholder='password' />
-        
+        <input className={classes.mainInput} type="password" placeholder='password' />
         <input className={classes.mainInput} type="password" placeholder='password' />
         <button className={classes.btnLogin}>Login</button>
 
