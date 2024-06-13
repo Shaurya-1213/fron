babf19d38be1acd07929b7bc04510986a74976da
Shaurya Singh <163731296+Shaurya-1213@users.noreply.github.com>
2024-06-13 11:01:11 -0400

    Update page.jsx

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,7 +4,8 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact</h1>
-      <h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1>
+      <h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1><h1>Contact</h1>
+      
     </div>
   )
 }
